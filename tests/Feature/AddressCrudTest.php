c399c9e28d11ab41cba3090074ac4878c7d75222
cronugs <cronugs@outlook.com>
2022-12-01 13:56:40 +1100

    address crud testing

--- tests/Feature/AddressCrudTest.php
+++ tests/Feature/AddressCrudTest.php
@@ -45,4 +45,42 @@ class AddressCrudTest extends TestCase
             'countryCode' => 'AU'
         ]);
     }
+
+    public function test_user_can_update_address()
+    {
+        // $user = User::factory()->create([
+        //     'password' => bcrypt('password')
+        // ]);
+
+        $user = User::find(12);
+
+        $address = Address::factory()->create(['user_id' => $user->id]);
+        
+        //$address = Address::find(49);
+        $response = $this->actingAs($user)->post('/api/addresses/update', [
+            'line1'  => 'test update address',
+            'line2' => 'Suite 1',
+            'line3' => 'Industrial Testing Park',
+            'line4' => 'Complex X',
+            'city' => 'Lismore',
+            'postalCode' => '2480',
+            'stateCode' => 'NSW',
+            'stateName' => 'New South Wales',
+            'countryCode' => 'AU',
+            'id' => $address->id,
+        ]);
+        $response->assertSessionHasNoErrors();
+        //dd(Address::find($address->id));
+        $this->assertEquals('test update address', Address::find($address->id)->line1);
+    }
+
+    // public function test_admin_can_delete_product()
+    // {
+    //     $admin = User::factory()->create(['is_admin' => 1]);
+    //    $product =  Product::factory()->create();
+    //    $this->assertEquals(1, Product::count());
+    //    $response = $this->actingAs($admin)->delete('/products/'. $product->id);
+    //    $response->assertStatus(302);
+    //    $this->assertEquals(0, Product::count());
+    // }
 }
